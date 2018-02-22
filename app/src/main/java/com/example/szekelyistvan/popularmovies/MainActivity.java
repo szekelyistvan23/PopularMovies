824a97c8e5debdaf17e9b25e17e82e3c9c5a8417
Szekely Istvan <steven.sekler@gmail.com>
2018-02-22 10:32:38 +0200

    API key added

--- app/src/main/java/com/example/szekelyistvan/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/szekelyistvan/popularmovies/MainActivity.java
@@ -5,6 +5,11 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    /* Put your own api key from themoviedb.org to gradle.properties in the following form
+    API_KEY = "your api key goes here" */
+    private static final String API_KEY = BuildConfig.API_KEY;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
