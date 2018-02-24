39f5b04158fc9486cc8a01f48f90e28fb036406b
Szekely Istvan <steven.sekler@gmail.com>
2018-02-24 11:59:51 +0200

    feat: JSON data download

--- app/src/main/java/com/example/szekelyistvan/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/szekelyistvan/popularmovies/MainActivity.java
@@ -1,18 +1,68 @@
 package com.example.szekelyistvan.popularmovies;
 
+import android.app.DownloadManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
 
     /* Put your own api key from themoviedb.org to gradle.properties in the following form
     API_KEY = "your api key goes here" */
     private static final String API_KEY = BuildConfig.API_KEY;
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+    /** Sets up a RecyclerView */
+    private void setupRecyclerView(){
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view_main);
+        mRecyclerView.setHasFixedSize(true);
+
+        mLayoutManager = new GridLayoutManager(this, 2);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+
+    }
+    /** Downloads JSON data from the Internet*/
+    private void downloadData(){
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="https://api.themoviedb.org/3/movie/popular?api_key=" + API_KEY + "&language=en-US&page=1";
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MainActivity.this,"Somenthing went wrong!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        queue.add(stringRequest);
+
+    }
+
 }
